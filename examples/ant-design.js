d7d90088b3b51fe9f30f8bba9a7069323418ca9d
dormi <andrey.d@fora-soft.com>
2018-04-17 17:02:48 +0300

    Updates

--- examples/ant-design.js
+++ examples/ant-design.js
@@ -1,114 +1,92 @@
-webpackJsonp([1],{
-
-/***/ 100:
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
-/***/ 65:
+webpackJsonp([0],[
+/* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(66);
+	module.exports = __webpack_require__(1);
 
 
 /***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
 
-/***/ 66:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits__ = __webpack_require__(4);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rc_dialog_assets_index_less__ = __webpack_require__(100);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rc_dialog_assets_index_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_rc_dialog_assets_index_less__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_react__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_react__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react_dom__ = __webpack_require__(12);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_react_dom__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__src_DialogWrap__ = __webpack_require__(61);
-
-
-
-/* eslint no-console:0 */
-
-
-
-// use import Dialog from 'rc-dialog'
-
-
-var MyControl = function (_React$Component) {
-    __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits___default()(MyControl, _React$Component);
-
-    function MyControl() {
-        __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default()(this, MyControl);
-
-        var _this = __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn___default()(this, _React$Component.apply(this, arguments));
-
-        _this.state = {
-            visible: false,
-            width: 600,
-            destroyOnClose: false,
-            center: false,
-            mousePosition: {}
-        };
-        _this.onClick = function (e) {
-            _this.setState({
-                mousePosition: {
-                    x: e.pageX,
-                    y: e.pageY
-                },
-                visible: true
-            });
-        };
-        _this.onClose = function (e) {
-            // console.log(e);
-            _this.setState({
-                visible: false
-            });
-        };
-        _this.onDestroyOnCloseChange = function (e) {
-            _this.setState({
-                destroyOnClose: e.target.checked
-            });
-        };
-        _this.changeWidth = function () {
-            _this.setState({
-                width: _this.state.width === 600 ? 800 : 600
-            });
-        };
-        _this.center = function (e) {
-            _this.setState({
-                center: e.target.checked
-            });
-        };
-        return _this;
-    }
-
-    MyControl.prototype.render = function render() {
-        var style = {
-            width: this.state.width
-        };
-        var wrapClassName = '';
-        if (this.state.center) {
-            wrapClassName = 'center';
-        }
-        var dialog = __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_6__src_DialogWrap__["a" /* default */], { visible: this.state.visible, wrapClassName: wrapClassName, animation: "zoom", maskAnimation: "fade", onClose: this.onClose, style: style, mousePosition: this.state.mousePosition, destroyOnClose: this.state.destroyOnClose }, __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("input", null), __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("p", null, "basic modal"), __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("button", { onClick: this.changeWidth }, "change width"), __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("div", { style: { height: 200 } }));
-        return __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("div", { style: { width: '90%', margin: '0 auto' } }, __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("style", null, '\n            .center {\n              display: flex;\n              align-items: center;\n              justify-content: center;\n            }\n            '), __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("p", null, __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("button", { className: "btn btn-primary", onClick: this.onClick }, "show dialog"), '\xA0', __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("label", null, "destroy on close:", __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("input", { type: "checkbox", checked: this.state.destroyOnClose, onChange: this.onDestroyOnCloseChange })), '\xA0', __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("label", null, "center", __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("input", { type: "checkbox", checked: this.state.center, onChange: this.center }))), dialog);
-    };
-
-    return MyControl;
-}(__WEBPACK_IMPORTED_MODULE_4_react___default.a.Component);
-
-__WEBPACK_IMPORTED_MODULE_5_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("div", null, __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement("h2", null, "ant-design dialog"), __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(MyControl, null)), document.getElementById('__react-content'));
+	'use strict';
+	
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-dialog/assets/index.less\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	
+	var _react = __webpack_require__(2);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactDom = __webpack_require__(35);
+	
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+	
+	var _DialogWrap = __webpack_require__(173);
+	
+	var _DialogWrap2 = _interopRequireDefault(_DialogWrap);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	/* eslint no-console:0 */
+	var MyControl = _react2.default.createClass({
+	    displayName: 'MyControl',
+	    getInitialState: function getInitialState() {
+	        return {
+	            visible: false,
+	            width: 600,
+	            destroyOnClose: false,
+	            center: false
+	        };
+	    },
+	    onClick: function onClick(e) {
+	        this.setState({
+	            mousePosition: {
+	                x: e.pageX,
+	                y: e.pageY
+	            },
+	            visible: true
+	        });
+	    },
+	    onClose: function onClose(e) {
+	        // console.log(e);
+	        this.setState({
+	            visible: false
+	        });
+	    },
+	    onDestroyOnCloseChange: function onDestroyOnCloseChange(e) {
+	        this.setState({
+	            destroyOnClose: e.target.checked
+	        });
+	    },
+	    changeWidth: function changeWidth() {
+	        this.setState({
+	            width: this.state.width === 600 ? 800 : 600
+	        });
+	    },
+	    center: function center(e) {
+	        this.setState({
+	            center: e.target.checked
+	        });
+	    },
+	    render: function render() {
+	        var dialog = void 0;
+	        if (this.state.visible || !this.state.destroyOnClose) {
+	            var style = {
+	                width: this.state.width
+	            };
+	            var wrapClassName = '';
+	            if (this.state.center) {
+	                wrapClassName = 'center';
+	            }
+	            dialog = _react2.default.createElement(_DialogWrap2.default, { visible: this.state.visible, wrapClassName: wrapClassName, animation: "zoom", maskAnimation: "fade", onClose: this.onClose, style: style, mousePosition: this.state.mousePosition }, _react2.default.createElement("input", null), _react2.default.createElement("p", null, "basic modal"), _react2.default.createElement("button", { onClick: this.changeWidth }, "change width"), _react2.default.createElement("div", { style: { height: 200 } }));
+	        }
+	        return _react2.default.createElement("div", { style: { width: '90%', margin: '0 auto' } }, _react2.default.createElement("style", null, '\n            .center {\n              display: flex;\n              align-items: center;\n              justify-content: center;\n            }\n            '), _react2.default.createElement("p", null, _react2.default.createElement("button", { className: "btn btn-primary", onClick: this.onClick }, "show dialog"), '\xA0', _react2.default.createElement("label", null, "destroy on close:", _react2.default.createElement("input", { type: "checkbox", checked: this.state.destroyOnClose, onChange: this.onDestroyOnCloseChange })), '\xA0', _react2.default.createElement("label", null, "center", _react2.default.createElement("input", { type: "checkbox", checked: this.state.center, onChange: this.center }))), dialog);
+	    }
+	});
+	// use import Dialog from 'rc-dialog'
+	
+	_reactDom2.default.render(_react2.default.createElement("div", null, _react2.default.createElement("h2", null, "ant-design dialog"), _react2.default.createElement(MyControl, null)), document.getElementById('__react-content'));
 
 /***/ })
-
-},[65]);
+]);
 //# sourceMappingURL=ant-design.js.map
--- examples/bootstrap.js
+++ examples/bootstrap.js
@@ -1,100 +1,77 @@
-webpackJsonp([0],{
+webpackJsonp([1],{
 
-/***/ 132:
+/***/ 0:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(133);
+	module.exports = __webpack_require__(279);
 
 
 /***/ }),
 
-/***/ 133:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ 279:
+/***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits__ = __webpack_require__(4);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_bootstrap_dist_css_bootstrap_css__ = __webpack_require__(134);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_bootstrap_dist_css_bootstrap_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_bootstrap_dist_css_bootstrap_css__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_rc_dialog_assets_bootstrap_less__ = __webpack_require__(135);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_rc_dialog_assets_bootstrap_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_rc_dialog_assets_bootstrap_less__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_react__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react_dom__ = __webpack_require__(12);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_6_react_dom__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__src_DialogWrap__ = __webpack_require__(61);
-
-
-
-
-
-
-
-
-
-var MyControl = function (_React$Component) {
-    __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits___default()(MyControl, _React$Component);
-
-    function MyControl() {
-        __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default()(this, MyControl);
-
-        var _this = __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn___default()(this, _React$Component.apply(this, arguments));
-
-        _this.state = {
-            visible: false,
-            destroyOnClose: false
-        };
-        _this.onClick = function () {
-            _this.setState({
-                visible: true
-            });
-        };
-        _this.onClose = function () {
-            _this.setState({
-                visible: false
-            });
-        };
-        _this.onDestroyOnCloseChange = function (e) {
-            _this.setState({
-                destroyOnClose: e.target.checked
-            });
-        };
-        return _this;
-    }
-
-    MyControl.prototype.render = function render() {
-        var dialog = void 0;
-        if (this.state.visible || !this.state.destroyOnClose) {
-            dialog = __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_7__src_DialogWrap__["a" /* default */], { visible: this.state.visible, animation: "slide-fade", maskAnimation: "fade", onClose: this.onClose, style: { width: 600 }, title: __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("div", null, '\u7B2C\u4E8C\u4E2A\u5F39\u6846'), footer: [__WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("button", { type: "button", className: "btn btn-default", key: "close", onClick: this.onClose }, "Close"), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("button", { type: "button", className: "btn btn-primary", key: "save", onClick: this.onClose }, "Save changes")] }, __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("h4", null, "Text in a modal"), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Duis mollis, est non commodo luctus, nisi erat porttitor ligula."), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("hr", null), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("h4", null, "Overflowing text to show scroll behavior"), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros."), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("div", { style: { display: '' } }, __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla."), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros."), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla."), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros."), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, "Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.")));
-        }
-        return __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("div", { style: { margin: 20 } }, __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("p", null, __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("button", { className: "btn btn-primary", onClick: this.onClick }, "show dialog"), '\xA0', __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("label", null, "destroy on close:", __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("input", { type: "checkbox", checked: this.state.destroyOnClose, onChange: this.onDestroyOnCloseChange }))), dialog);
-    };
-
-    return MyControl;
-}(__WEBPACK_IMPORTED_MODULE_5_react___default.a.Component);
-
-__WEBPACK_IMPORTED_MODULE_6_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("div", null, __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement("h2", null, "ant-design dialog"), __WEBPACK_IMPORTED_MODULE_5_react___default.a.createElement(MyControl, null)), document.getElementById('__react-content'));
+	'use strict';
+	
+	__webpack_require__(280);
+	
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-dialog/assets/bootstrap.less\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	
+	var _react = __webpack_require__(2);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactDom = __webpack_require__(35);
+	
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+	
+	var _DialogWrap = __webpack_require__(173);
+	
+	var _DialogWrap2 = _interopRequireDefault(_DialogWrap);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	var MyControl = _react2.default.createClass({
+	    displayName: 'MyControl',
+	    getInitialState: function getInitialState() {
+	        return {
+	            visible: false,
+	            destroyOnClose: false
+	        };
+	    },
+	    onClick: function onClick() {
+	        this.setState({
+	            visible: true
+	        });
+	    },
+	    onClose: function onClose() {
+	        this.setState({
+	            visible: false
+	        });
+	    },
+	    onDestroyOnCloseChange: function onDestroyOnCloseChange(e) {
+	        this.setState({
+	            destroyOnClose: e.target.checked
+	        });
+	    },
+	    render: function render() {
+	        var dialog = void 0;
+	        if (this.state.visible || !this.state.destroyOnClose) {
+	            dialog = _react2.default.createElement(_DialogWrap2.default, { visible: this.state.visible, animation: "slide-fade", maskAnimation: "fade", onClose: this.onClose, style: { width: 600 }, title: _react2.default.createElement("div", null, '\u7B2C\u4E8C\u4E2A\u5F39\u6846'), footer: [_react2.default.createElement("button", { type: "button", className: "btn btn-default", key: "close", onClick: this.onClose }, "Close"), _react2.default.createElement("button", { type: "button", className: "btn btn-primary", key: "save", onClick: this.onClose }, "Save changes")] }, _react2.default.createElement("h4", null, "Text in a modal"), _react2.default.createElement("p", null, "Duis mollis, est non commodo luctus, nisi erat porttitor ligula."), _react2.default.createElement("hr", null), _react2.default.createElement("h4", null, "Overflowing text to show scroll behavior"), _react2.default.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum." + " " + "Cras justo odio, dapibus ac facilisis in," + " " + "egestas eget quam. Morbi leo risus, porta ac consectetur ac," + " " + "vestibulum at eros."), _react2.default.createElement("p", null, "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus" + " " + "sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."), _react2.default.createElement("div", { style: { display: '' } }, _react2.default.createElement("p", null, "Aenean lacinia bibendum nulla sed" + " " + "consectetur. Praesent commodo cursus magna," + " " + "vel scelerisque nisl consectetur et. Donec sed odio dui." + " " + "Donec" + " " + "ullamcorper nulla non metus auctor fringilla."), _react2.default.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum." + " " + "Cras justo odio, dapibus ac facilisis in, egestas" + " " + "eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros."), _react2.default.createElement("p", null, "Praesent commodo cursus" + " " + "magna, vel scelerisque nisl consectetur et." + " " + "Vivamus sagittis lacus vel augue laoreet rutrum faucibus" + " " + "dolor auctor."), _react2.default.createElement("p", null, "Aenean lacinia bibendum nulla sed consectetur." + " " + "Praesent commodo cursus magna, vel" + " " + "scelerisque nisl consectetur et. Donec sed odio dui." + " " + "Donec ullamcorper nulla non metus auctor" + " " + "fringilla."), _react2.default.createElement("p", null, "Cras mattis consectetur purus sit amet fermentum." + " " + "Cras justo odio, dapibus ac" + " " + "facilisis in, egestas eget quam. Morbi leo risus," + " " + "porta ac consectetur ac, vestibulum at eros."), _react2.default.createElement("p", null, "Praesent commodo cursus magna, vel scelerisque nisl consectetur et." + " " + "Vivamus sagittis lacus vel augue" + " " + "laoreet rutrum faucibus dolor auctor."), _react2.default.createElement("p", null, "Aenean lacinia bibendum nulla sed consectetur. Praesent" + " " + "commodo cursus magna, vel scelerisque nisl consectetur et." + " " + "Donec sed odio dui. Donec ullamcorper nulla" + " " + "non metus auctor fringilla.")));
+	        }
+	        return _react2.default.createElement("div", { style: { margin: 20 } }, _react2.default.createElement("p", null, _react2.default.createElement("button", { className: "btn btn-primary", onClick: this.onClick }, "show dialog"), '\xA0', _react2.default.createElement("label", null, "destroy on close:", _react2.default.createElement("input", { type: "checkbox", checked: this.state.destroyOnClose, onChange: this.onDestroyOnCloseChange }))), dialog);
+	    }
+	});
+	_reactDom2.default.render(_react2.default.createElement("div", null, _react2.default.createElement("h2", null, "ant-design dialog"), _react2.default.createElement(MyControl, null)), document.getElementById('__react-content'));
 
 /***/ }),
 
-/***/ 134:
+/***/ 280:
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: ExtractPluginMissingException: svg-sprite-loader exception. svg-sprite-loader in extract mode requires the corresponding plugin\n    at Object.loader (/Users/meck/Workspace/react-component/packages/rc-dialog/node_modules/svg-sprite-loader/lib/loader.js:54:13)\n    at runLoaders (/Users/meck/Workspace/react-component/packages/rc-dialog/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/meck/Workspace/react-component/packages/rc-dialog/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/meck/Workspace/react-component/packages/rc-dialog/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at runSyncOrAsync (/Users/meck/Workspace/react-component/packages/rc-dialog/node_modules/loader-runner/lib/LoaderRunner.js:143:3)\n    at iterateNormalLoaders (/Users/meck/Workspace/react-component/packages/rc-dialog/node_modules/loader-runner/lib/LoaderRunner.js:229:2)\n    at /Users/meck/Workspace/react-component/packages/rc-dialog/node_modules/loader-runner/lib/LoaderRunner.js:202:4\n    at /Users/meck/Workspace/react-component/packages/rc-dialog/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:70:14\n    at _combinedTickCallback (internal/process/next_tick.js:131:7)\n    at process._tickCallback (internal/process/next_tick.js:180:9)");
-
-/***/ }),
-
-/***/ 135:
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
+	// removed by extract-text-webpack-plugin
 
 /***/ })
 
-},[132]);
+});
 //# sourceMappingURL=bootstrap.js.map
